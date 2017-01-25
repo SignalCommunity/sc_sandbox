1fda1769a7ac03b6fb56bec437b00876bb259f99
Bismarck <pankgaming@gmail.com>
2017-01-24 19:28:53 -0500

    Working directories created

---
+++ config.cpp
@@ -0,0 +1,24 @@
+
+class CfgPatches
+{
+	class sc_sandbox
+	{
+		author = "Bismarck";
+		name = "SC Sandbox";
+		url = "www.signalcommunity.com";
+		requiredVersion = 0.1;
+	};
+};
+
+
+class CfgMissions
+{
+	class MPMissions
+	{
+		class sc_sandbox_stratis
+		{
+			briefingName = "~~SC Sandbox~~";
+			directory = "sc_sandbox\mpscenarios\sc_sandbox.Stratis";
+		};
+	};
+};
